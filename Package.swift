6dfa66fbfb1b6ecf586f683522ce24fd73fd6d6c
Chris <csjones@users.noreply.github.com>
2018-06-08 16:38:54 -0700

    Updating package.swift

--- Package.swift
+++ Package.swift
@@ -22,6 +22,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "libbtc",
-            dependencies: []),
+            dependencies: ["liblogdb"]),
     ]
 )
