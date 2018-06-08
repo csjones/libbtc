cfff650bb5f6cf80a78564cce63a8d6d3ca3980d
Chris <csjones@users.noreply.github.com>
2018-06-08 16:47:06 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,13 +15,13 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "http://github.com/csjones/Clibevent", .branch("master")),
-        .package(url: "http://github.com/csjones/liblogdb", .branch("SWIFT")),
+        //.package(url: "http://github.com/csjones/liblogdb", .branch("SWIFT")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "libbtc",
-            dependencies: ["liblogdb"]),
+            dependencies: []),
     ]
 )
