62e89050d1203b9ffc4422a361f09e6a3c580f19
Chris <csjones@users.noreply.github.com>
2018-06-08 12:30:16 -0700

    Adding Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "libbtc",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "libbtc",
+            targets: ["libbtc"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "http://github.com/csjones/Clibevent", .branch("master")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "libbtc",
+            dependencies: []),
+    ]
+)
